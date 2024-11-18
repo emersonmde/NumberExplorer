c13289828ee0af7facac033f8fd91084eb546e2e
Matthew Emerson <emersonmde@protonmail.com>
2024-11-18 12:26:24 -0500

    Initial Commit

---
+++ NumberExplorer/NumberExplorerApp.swift
@@ -0,0 +1,32 @@
+//
+//  NumberExplorerApp.swift
+//  NumberExplorer
+//
+//  Created by Matthew Emerson on 11/18/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NumberExplorerApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
